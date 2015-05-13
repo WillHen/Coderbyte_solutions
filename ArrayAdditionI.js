7a08b1c14727b6a26768cb457626dda9a362bfbf
WillHen <whenshaw87@googlemail.com>
2015-05-13 21:00:52 +0100

    Delete ArrayAdditionI.js

--- ArrayAdditionI.js
+++
@@ -1,9 +0,0 @@
-function ArrayAdditionI(arr) { 
-var max = arr.sort(function(a, b){return a-b}).pop([arr.length-1])
-var sum = arr.reduce(function(x,y){return x +y;})
-for(i = 0; i < arr.length; i++){
-  if(sum - arr[i] == max){ return true}
-}
-  return false
-}
-   
