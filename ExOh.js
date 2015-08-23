ab18c6007cb8959379c591ad3becd1e2d17e24fd
Will H <whenshaw87@googlemail.com>
2015-08-23 10:49:43 +0100

    Refactored ExOh solution

--- ExOh.js
+++ ExOh.js
@@ -1,15 +1,7 @@
 function ExOh(str) { 
- var arr = str.split("");
-  var countX = 0
-      var countO = 0
-      
-  for(i = 0; i < arr.length; i ++){
-    if(arr[i] == "x") {
-      countX++}
-    else { countO++}
-  }
-    if(countX == countO) {
-      return true}
-    else{ return false} ;
-         
+ var arr = str.split(""), countX = 0, countO = 0;
+  arr.forEach(function(ele){
+  ele === "o" ? countO++ : countX++;
+ })
+return countX === countO;         
 }
